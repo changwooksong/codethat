d893d9a2c1596313c50e6ac3f4eaba68abba5394
changwook <scw9327@naver.com>
2022-12-02 22:41:12 +0900

    09. 하위 페이지 나누기

--- src/Main.js
+++ src/Main.js
@@ -8,17 +8,21 @@ import WishlistPage from "./pages/WishlistPage";
 function Main() {
     return (
         <BrowserRouter>
-            <App>
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="courses" element={<CourseListPage />} />
-                    <Route
-                        path="courses/react-frontend-development"
-                        element={<CoursePage />}
-                    />
+            {/* <App> */}
+            <Routes>
+                <Route path="/" element={<App />}>
+                    <Route index element={<HomePage />} />
+                    <Route path="courses">
+                        <Route index element={<CourseListPage />} />
+                        <Route
+                            path="react-frontend-development"
+                            element={<CoursePage />}
+                        />
+                    </Route>
                     <Route path="wishlist" element={<WishlistPage />} />
-                </Routes>
-            </App>
+                </Route>
+            </Routes>
+            {/* </App> */}
         </BrowserRouter>
     );
 }
